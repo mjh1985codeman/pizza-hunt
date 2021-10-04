bd22f14fc96c3e8a59ecb947de2ea95f4fd29a4e
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-10-03 20:27:21 -0500

    created comment model and tied it to the front end as well

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,8 @@
-const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
+const router = require("express").Router();
+const commentRoutes = require("./comment-routes");
+const pizzaRoutes = require("./pizza-routes");
 
-router.use('/pizzas', pizzaRoutes);
+router.use("/comments", commentRoutes);
+router.use("/pizzas", pizzaRoutes);
 
 module.exports = router;
